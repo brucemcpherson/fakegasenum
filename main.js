1c8eb2e934dd0f7d4ac44681fc3e0388a7d66ae1
Bruce Mcpherson <bruce@mcpher.com>
2025-06-03 16:03:29 +0100

    init

---
+++ main.js
@@ -0,0 +1,1 @@
+export { newFakeGasenum } from "./src/Gasenum.js"
---
+++ togas.sh
@@ -0,0 +1,16 @@
+SOURCE="./src"
+EXT="*.js"
+CLASP=true
+
+TARGET="./bmFakeGasenum"
+cp "${SOURCE}"/${EXT} "${TARGET}/"
+# find all the copied files and comment/fixes out import and export statements
+# note - this simple version naively expects that to be on 1 line
+sed -i 's/^import\s\s*/\/\/import /g' $(find "${TARGET}" -name "$EXT" -type f) 
+sed -i 's/^\s*export\s\s*//g' $(find "${TARGET}" -name "$EXT" -type f)
+
+# now go to the target and push and open if required
+if [ "$CLASP" = true ] ; then
+  cd "${TARGET}"
+  clasp push
+fi
